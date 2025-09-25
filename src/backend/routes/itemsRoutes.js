b83ef0d638130fcdd715274415414e4df23371b9
Camila Galieta Bernardes <camilagalietaber@hotmail.com>
2025-09-25 10:04:01 -0300

    mudanças

--- src/backend/routes/itemsRoutes.js
+++ src/backend/routes/itemsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const itemsController = require('../controllers/itemsController');
+const itemsController = require('../backend/controllers/itemsController');
 
 router.get('/items', itemsController.getItems);
 router.get('/items/:id', itemsController.getItemById);
